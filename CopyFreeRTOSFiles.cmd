8e1e7cd35be53e920207fd5e18d50a4bc24d7bf4
xlfdll <xlfdll@gmail.com>
2019-11-24 20:36:18 -0800

    Added FreeRTOS file copy script

---
+++ CopyFreeRTOSFiles.cmd
@@ -0,0 +1,33 @@
+@echo off
+
+IF EXIST %1\FreeRTOS (
+    pushd FreeRTOS
+    xcopy %1\FreeRTOS\Source\*.c . /y
+    
+    pushd include
+    xcopy %1\FreeRTOS\Source\include\*.* . /y
+    
+    pushd portable\PIC32MX
+    xcopy %1\FreeRTOS\Source\portable\MPLAB\PIC32MX\*.h . /y
+    xcopy %1\FreeRTOS\Demo\PIC32MX_MPLAB\ConfigPerformance.h . /y
+    xcopy %1\FreeRTOS\Demo\PIC32MX_MPLAB\FreeRTOSConfig.h . /y
+    popd
+    
+    popd
+    
+    pushd memory
+    xcopy %1\FreeRTOS\Source\portable\MemMang\heap_4.c . /y
+    popd
+    
+    pushd portable\PIC32MX
+    xcopy %1\FreeRTOS\Source\portable\MPLAB\PIC32MX\*.c . /y
+    xcopy %1\FreeRTOS\Source\portable\MPLAB\PIC32MX\*.S . /y
+    xcopy %1\FreeRTOS\Demo\PIC32MX_MPLAB\ConfigPerformance.c . /y
+    popd
+    
+    pushd util
+    xcopy %1\FreeRTOS\Demo\PIC32MX_MPLAB\printf-stdarg.c . /y
+    popd
+    
+    popd
+)
